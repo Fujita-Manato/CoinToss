244c6484b4cce29a28f97b67bda5d33e239bb1c9
FUJITA Manato <s1290164@sshsv211.u-aizu.ac.jp>
2022-07-10 14:23:24 +0900

    HoT: initial revision

---
+++ CoinToss.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+class CoinToss{
+    public static void main(String args[]) {
+        Random rand = new Random();
+       
+        int count1=0;
+        int count2=0;
+        
+        System.out.println("Tossing a coin...");
+        for(int i=0;i<3;i++) {
+            if(rand.nextInt(2)==0) {
+                System.out.println("Round "+i+": Heads");
+                count1++;
+            }
+            else{
+                System.out.println("Round "+i+": Tails");
+                count2++;
+            }
+            
+        }
+        System.out.println("Heads: "+count1+", Tails: "+count2);
+       /* if(count1>count2) {
+            System.out.println("You won");
+        }
+        else {
+            System.out.println("You lost");
+        }*/
+    }
+}
